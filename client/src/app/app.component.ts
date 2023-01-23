2877be6835db5159ecee4f713b09ff50ccaa3f0d
Ernest <ernesthodge@yahoo.com>
2023-01-22 21:47:13 -0600

    End of Section 11

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,8 +15,6 @@ export class AppComponent implements OnInit {
     this.setCurrentUser();
   }
 
-
-
   setCurrentUser(){
 
     const userString = localStorage.getItem('user');
@@ -25,8 +23,6 @@ export class AppComponent implements OnInit {
 
     const user: User = JSON.parse(userString);
     this.accountService.setCurrentUser(user);
-
-
 
   }
 
